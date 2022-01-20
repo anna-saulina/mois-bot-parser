f87988b2791474a49fc425b4976d64d91c7f2821
anna-saulina <ann.saulina@gmail.com>
2022-01-21 01:36:45 +0300

    add scraper for okey

--- scraper_okey_selenium.py
+++ scraper_okey_selenium.py
@@ -1,15 +1,12 @@
 import json
 import time
-from decimal import Decimal
 from random import randint
 
 from pprint import pprint
 
 from selenium.common.exceptions import NoSuchElementException
-
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service
-
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.safari.options import Options as SafariOptions
 from selenium import webdriver
@@ -17,14 +14,14 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-from services import extract_ozon_product_weight
+from services import parse_price, extract_okey_product_weight, GRAM, find_minimum_price
 
 
 def scrape_ozon_price():
-    url = 'https://www.ozon.ru/category/kofe-9372/?page=2'
-
-    # service = Service(executable="/usr/local/bin/geckodriver")
+    url = 'https://www.okeydostavka.ru/spb/goriachie-i-kholodnye-napitki/kofe-kakao-tsikorii/kofe#facet' \
+          ':-7000000000000024651104232107910771088108510721093,' \
+          '-70000000000000246511052108610831086109010991081&productBeginIndex:0&orderBy:&pageView:grid&minPrice' \
+          ':&maxPrice:&pageSize:& '
 
     # Safari,  run on Mac only
     # safari_options = SafariOptions()
@@ -45,50 +42,68 @@ def scrape_ozon_price():
 
     driver.get(url)
     html = driver.page_source
-    print(html)
+    # print(html)
 
     # sleep for two second then scroll to the bottom of the page
     time.sleep(randint(3, 4))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(randint(4, 5))  # sleep between loadings
 
-    unit_blocks = driver.find_elements(By.CSS_SELECTOR, value='.hj1')
-
-    # unit_blocks = soup.select('div.hj0.hj1')
-    print(f'Unit block length: {len(unit_blocks)}')
-    pprint(f'Unit block: {unit_blocks[0]}')
+    unit_blocks = driver.find_elements(By.CSS_SELECTOR, value='.product.ok-theme')
 
     product_list = []
 
     for unit in unit_blocks:
         try:
-            price = unit.find_element(By.CSS_SELECTOR, value='.ui-p9.ui-q1.ui-q4').text.split()
+            price = unit.find_element(By.TAG_NAME, value='input').get_attribute('value').split()
+            # print(price)
         except NoSuchElementException:
             price = None
 
         try:
-            title = unit.find_element(By.CSS_SELECTOR, value='.c8i.i8c.c9i.jc0.f-tsBodyL.gz9').text
+            title = unit.find_element(By.TAG_NAME, value='a').get_attribute('title')
         except NoSuchElementException:
             title = None
 
         try:
-            link = unit.find_element(By.CSS_SELECTOR, value='.tile-hover-target.gz9').get_attribute('href')
+            link = unit.find_element(By.TAG_NAME, value='a').get_attribute('href')
         except NoSuchElementException:
             link = None
 
-        weight_measure = extract_ozon_product_weight(title)
+        if price:
+            converted_price = parse_price(price)
+            if len(price) == 2:
+                currency = price[1]
+            if len(price) == 3:
+                currency = price[2]
+        else:
+            converted_price = None
+            currency = None
+
+        weight_measure = extract_okey_product_weight(title)
+
+        if weight_measure[0] and weight_measure[1]:
+            if weight_measure[1] in GRAM:
+                price_kg = converted_price / weight_measure[0] * 1000
+            else:
+                price_kg = converted_price
+        else:
+            price_kg = None
 
         unit_dict = {
             'title': title,
-            'price': float(round(Decimal(price[0]), 2)) if price else None,
-            'currency': price[1] if price else None,
+            'price': converted_price if converted_price else None,
+            'price_kg': price_kg,
+            'currency': currency if currency else None,
             'link': link,
-            'weight': weight_measure[0],
-            'measure': weight_measure[1]
+            'weight': weight_measure[0] if weight_measure else None,
+            'measure': weight_measure[1] if weight_measure else None
         }
         product_list.append(unit_dict)
 
     pprint(product_list)
+
+    minimum_price_product = find_minimum_price(product_list)
 
     driver.quit()
 
@@ -96,13 +111,16 @@ def scrape_ozon_price():
     print(json_string)
 
     if len(product_list) > 0:
-        with open('./ozon_price.json', 'w', encoding='utf-8') as outfile:
+        with open('./okey_price.json', 'w', encoding='utf-8') as outfile:
             json.dump(product_list, outfile, sort_keys=True, indent=4, ensure_ascii=False)
 
-    return product_list
+    if minimum_price_product:
+        with open('./okey_minimum_price.json', 'w', encoding='utf-8') as outfile:
+            json.dump(minimum_price_product, outfile, sort_keys=True, indent=4, ensure_ascii=False)
+
+    return minimum_price_product
 
 
 if __name__ == '__main__':
     result = scrape_ozon_price()
-
-
+    print(result)
--- services.py
+++ services.py
@@ -1,9 +1,11 @@
 # выделение массы продукта из строки
 import json
-
+import math
 
 # CONSTANTS
 MEASURE = ('кг', 'кг.', 'килограмм', 'г', 'гр', 'г.', 'гр.', 'грамм')
+KILOGRAM = ('кг', 'кг.', 'килограмм')
+GRAM = ('г', 'гр', 'г.', 'гр.', 'грамм')
 
 
 def load_json(json_file):
@@ -12,7 +14,7 @@ def load_json(json_file):
         return data
 
 
-def extract_product_weight(title):
+def extract_ozon_product_weight(title):
     title_list = title.split(',')
     # print(title_list)
     if title and len(title_list) > 1:
@@ -36,14 +38,103 @@ def extract_product_weight(title):
             return weight_measure
 
 
+def extract_okey_product_weight(title=None):
+    try:
+        title_list = title.split()
+    except AttributeError:
+        title_list = []
+
+    weight = 0
+    measure = ''
+
+    # print(title_list)
+    if title_list and len(title_list) > 1:
+        try:
+            weight = float(title_list[-1][0:-1])
+            measure = title_list[-1][-1]
+        except ValueError:
+            weight = None
+            measure = None
+
+    if weight is None:
+        try:
+            weight = float(title_list[-1][0:-2])
+            measure = title_list[-1][-2:]
+        except ValueError:
+            weight = None
+            measure = None
+
+    if weight is None:
+        try:
+            weight = float(title_list[-2][0:-1])
+            measure = title_list[-2][-1]
+        except:
+            weight = None
+            measure = None
+        # print(weight_list)
+
+    if measure and measure.lower() in MEASURE:
+        measure = measure.lower()
+    else:
+        measure = None
+
+    weight_measure = (weight, measure)
+    # print(weight, measure)
+
+    return weight_measure
+
+
+def parse_price(price_data):
+    converted_price = 0.0
+    if price_data:
+        if len(price_data) == 2:
+            price_list = price_data[0].split(',')
+            integer_part_hundred = float(price_list[0])
+            decimal_part = float(price_list[1]) / 100
+            converted_price = integer_part_hundred + decimal_part
+        if len(price_data) == 3:
+            price_list = price_data[1].split(',')
+            integer_part_thousand = float(price_data[0]) * 1000
+            integer_part_hundred = float(price_list[0])
+            decimal_part = float(price_list[1]) / 100
+            converted_price = integer_part_thousand + integer_part_hundred + decimal_part
+
+        # print(f'Converted price: {converted_price}')
+        return converted_price
+
+
+def find_minimum_price(product_data):
+    minimum_price_product = {}
+    minimum_price = math.inf
+    for product in product_data:
+        if product['price_kg'] and product['price_kg'] > 0:
+            if product['price_kg'] < minimum_price:
+                minimum_price_product = product
+                minimum_price = product['price_kg']
+
+    return minimum_price_product
 
 
 if __name__ == '__main__':
-    price_list = load_json('./ozon_price.json')
+    # price_list = load_json('./ozon_price.json')
+    # print(price_list)
+    #
+    # for element in price_list:
+    #     title = element['title']
+    #     weight_measure = extract_ozon_product_weight(title)
+    #     print(weight_measure)
+    #     # print(title)
+
+    price_list = load_json('./okey_price.json')
     print(price_list)
 
-    for element in price_list:
-        title = element['title']
-        weight_measure = extract_product_weight(title)
-        print(weight_measure)
-        # print(title)
+    # for element in price_list:
+    #     title = element['title']
+    #     weight_measure = extract_okey_product_weight(title)
+    #     print(weight_measure)
+
+    print(find_minimum_price(price_list))
+
+
+    # parse_price(['1', '899,00', '₽'])
+    # parse_price(['899,00', '₽'])
